165b10eebcd99c26a477d9dc5b20e312bdaf1b06
Mones nazih ksasbeh <157751352+Mones-ksasbeh@users.noreply.github.com>
2025-02-21 01:02:11 +0300

    Update WebApp.py

--- WebApp.py
+++ WebApp.py
@@ -232,10 +232,17 @@ elif option == "Explainable AI (XAI)":
     st.markdown("<p style= font-family: 'Times New Roman'> This section will display the <b> feature importance </b> for the <b> kidney disease prediction model</b> , highlighting which features contribute the most to the model's decision-making. This helps in understanding the impact of different medical parameters, such as blood pressure, serum creatinine, and hemoglobin levels, on the prediction..</p><br>", unsafe_allow_html=True)
     st.markdown("<p style= font-family: 'Times New Roman'>Additionally, this section will include the <b>Grad-CAM heatmap</b> for <b>CT images</b>, providing a visual explanation of which regions in the image were most influential in the model's classification. This enhances interpretability by showing areas of interest for diagnosing kidney conditions such as tumors, cysts, or stones.</p>", unsafe_allow_html=True)
 
-    # Create SHAP Explainer
-    explainer = shap.TreeExplainer(ada_model)
-    shap_values = explainer(Data)
+    # Use a subset of data for background (for faster computation)
+    background = shap.sample(Data, 100)  # Sample 100 instances
 
+    # Create SHAP KernelExplainer
+    explainer = shap.KernelExplainer(ada_model.predict_proba, background)
+    
+    # Calculate SHAP values for a specific instance (e.g., first instance in the dataset)
+    instance = Data.iloc[0:1]  # Replace with the instance you want to explain
+    shap_values = explainer.shap_values(instance)
+
+    
     # Waterfall plot (only for one record)
     st.subheader("Feature Importance (Waterfall Plot)")
     fig, ax = plt.subplots(figsize=(10, 5))
