b241ef4b917d521504bb22e34c93194eddbf0735
Mones nazih ksasbeh <157751352+Mones-ksasbeh@users.noreply.github.com>
2025-02-22 20:52:10 +0300

    Update WebApp.py

--- WebApp.py
+++ WebApp.py
@@ -214,8 +214,6 @@ elif option == "Kidney Disease Prediction":
             
             # Display the prediction result
             if prediction[0] == 1:
-                st.markdown("<h5 style='font-family: Times New Roman;'>The model indicates a likelihood of Chronic Kidney Disease (CKD). Further clinical evaluation is recommended.</h5>", unsafe_allow_html=True)
-
                 shap_values = ada_model_XAI.shap_values(processed_input_data)  # Data is your input dataset (features only)
                
                 # Waterfall plot (only for one record)
@@ -235,7 +233,10 @@ elif option == "Kidney Disease Prediction":
                 fig, ax = plt.subplots(figsize=(10, 5))
                 shap.summary_plot(shap_values, X, show=False)
                 st.pyplot(fig)
+            
                 st.markdown("<h5 style='font-family: Times New Roman;'>The model indicates a likelihood of Chronic Kidney Disease (CKD). Further clinical evaluation is recommended.</h5>", unsafe_allow_html=True)
+
+               
 
             else:
                 st.markdown("<h5 style='font-family: Times New Roman;'>No significant indicators of Chronic kidney disease (CKD) detected. However, clinical judgment and further assessment may be required.</h5>", unsafe_allow_html=True)
