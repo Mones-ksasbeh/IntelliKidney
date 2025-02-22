2e0fa0e1fcbf06676890894361209be11b9f339b
Mones nazih ksasbeh <157751352+Mones-ksasbeh@users.noreply.github.com>
2025-02-22 20:50:43 +0300

    Update WebApp.py

--- WebApp.py
+++ WebApp.py
@@ -235,6 +235,8 @@ elif option == "Kidney Disease Prediction":
                 fig, ax = plt.subplots(figsize=(10, 5))
                 shap.summary_plot(shap_values, X, show=False)
                 st.pyplot(fig)
+                st.markdown("<h5 style='font-family: Times New Roman;'>The model indicates a likelihood of Chronic Kidney Disease (CKD). Further clinical evaluation is recommended.</h5>", unsafe_allow_html=True)
+
             else:
                 st.markdown("<h5 style='font-family: Times New Roman;'>No significant indicators of Chronic kidney disease (CKD) detected. However, clinical judgment and further assessment may be required.</h5>", unsafe_allow_html=True)
 
