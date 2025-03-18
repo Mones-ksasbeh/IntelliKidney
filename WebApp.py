33105564bcf7191a3f3d531e4dd001e4a9e59ef0
Mones nazih ksasbeh <157751352+Mones-ksasbeh@users.noreply.github.com>
2025-03-18 22:15:53 +0300

    Update WebApp.py

--- WebApp.py
+++ WebApp.py
@@ -246,7 +246,7 @@ elif option == "Kidney Disease Prediction":
             explanation_text = ''
             for feature in top_features:
                 explanation_text += f"- {new_record_df.columns[feature]} (Impact: {shap_values.values[0][feature]:.2f})\n"
-                st.success(explanation_text)
+                
             # Display the prediction result
             if prediction[0] == 1:
                 st.markdown("<h5 style='font-family: Times New Roman;'>The model indicates a likelihood of Chronic Kidney Disease (CKD). This diagnosis is influenced by: </h5>", unsafe_allow_html=True)
