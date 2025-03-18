2a38c82e2bd686fee0860934df42475c3edd9ae8
Mones nazih ksasbeh <157751352+Mones-ksasbeh@users.noreply.github.com>
2025-03-18 23:58:14 +0300

    Update WebApp.py

--- WebApp.py
+++ WebApp.py
@@ -360,6 +360,7 @@ with col3:
 with col4:
     Author_button = st.button("Author's")
 
+st.write('\n\n')
 # Display the contact information when the "Contact" button is clicked
 if Author_button:
     st.markdown(
